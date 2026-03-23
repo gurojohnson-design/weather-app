3ecd55cc757056bf04aada3e52a80633c530532a
August Johnson <gurojohnson@gmail.com>
2026-03-23 16:55:48 -0400

    Render DOM and update text content from data Also plug renderDOM() into async function to hit everything in one go

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const searchBar = document.getElementById('searchBar');
 async function getWeather(location) {
     try {
         // const location = searchBar.value;
-        let weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=QKH7NRC95WQ4AVCEV5HBRJABF&contentType=json`);
+        let weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=QKH7NRC95WQ4AVCEV5HBRJABF&contentType=json`);
         let response = await weather.json();
 
+        renderDOM(response);
+
         console.log(response);
+        console.log(response.resolvedAddress);
         return response;
     } catch (error) {
     console.log('Catch error: ', error);
@@ -23,33 +26,35 @@ searchBtn.addEventListener('click', (e) => {
   searchBar.value = '';
 });
 
-
-function renderDOM() {
+// create dom
+function renderDOM(response) {
     // time for background display, location, verbal description, temp, feels like temp, wind, humidity
 
     const content = document.getElementById('content');
 
     const location = document.createElement('div') ;
     location.id = 'location';
+    location.textContent = response.resolvedAddress;
 
     const verbDesc = document.createElement('div');
     verbDesc.id = 'verDesc';
-
-    const time = document.createElement('div');
-    time.id = 'time';
+    verbDesc.textContent = response.currentConditions.conditions;
 
     const temp = document.createElement('div');
     temp.id = 'temp';
+    temp.textContent = `${response.currentConditions.temp} \u00B0F`;
 
     const feelsLike = document.createElement('div');
     feelsLike.id = 'feelsLike';
+    feelsLike.textContent = `${response.currentConditions.feelslike} \u00B0F`;
 
     const wind = document.createElement('div');
     wind.id = 'wind';
+    wind.textContent = `${response.currentConditions.windspeed} mph`;
 
     const humidity = document.createElement('div');
     humidity.id = 'humidity';
+    humidity.textContent = `${response.currentConditions.humidity}%`;
 
-    content.append(location, verbDesc, time, temp, feelsLike, wind, humidity);
+    content.append(location, verbDesc, temp, feelsLike, wind, humidity);
 }
-
