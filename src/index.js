d35d3c02c768d5f02cc52f76be4eb1d54a5b6b78
August Johnson <gurojohnson@gmail.com>
2026-03-23 16:19:58 -0400

    Polish off getWeather() and write renderDOM()

--- src/index.js
+++ src/index.js
@@ -1,48 +1,55 @@
-/*
-const img = document.querySelector('img');
 const searchBtn = document.getElementById('searchBtn');
 const searchBar = document.getElementById('searchBar');
 
-function displayImage() {
-  const searchTerm = searchBar.value
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=erZiZzIv8Xlemiq4AOSV3KCHjIPiTzN3&s=${searchTerm}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      if (!response.data || !response.data.images) {
-        console.log('No image data returned:', response);
-        return;
-      }
-      img.src = response.data.images.original.url;
-    })
-    .catch(function(error) {
-      console.log('Fetch failed: ', error);
-    });
-}
-
-searchBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  displayImage();
-  searchBar.value = '';
-});
-*/
 
 async function getWeather(location) {
     try {
         // const location = searchBar.value;
-        let weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QKH7NRC95WQ4AVCEV5HBRJABF`);
+        let weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=QKH7NRC95WQ4AVCEV5HBRJABF&contentType=json`);
         let response = await weather.json();
 
-        if (!response.data) {
-            console.log('No data returned: ', response);
-            return;
-    }
         console.log(response);
+        return response;
     } catch (error) {
     console.log('Catch error: ', error);
 }
 };
 
 getWeather('london');
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  searchBar.value = '';
+});
+
+
+function renderDOM() {
+    // time for background display, location, verbal description, temp, feels like temp, wind, humidity
+
+    const content = document.getElementById('content');
+
+    const location = document.createElement('div') ;
+    location.id = 'location';
+
+    const verbDesc = document.createElement('div');
+    verbDesc.id = 'verDesc';
+
+    const time = document.createElement('div');
+    time.id = 'time';
+
+    const temp = document.createElement('div');
+    temp.id = 'temp';
+
+    const feelsLike = document.createElement('div');
+    feelsLike.id = 'feelsLike';
+
+    const wind = document.createElement('div');
+    wind.id = 'wind';
+
+    const humidity = document.createElement('div');
+    humidity.id = 'humidity';
+
+    content.append(location, verbDesc, time, temp, feelsLike, wind, humidity);
+}
+
